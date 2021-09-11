150bd301f7e344556c1ababb4bf2fd0b06d8eed9
David <david2000wang@gmail.com>
2021-09-11 16:39:12 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import { ReactComponent as Twitter } from './img/twitter.svg';
+import { ReactComponent as Github } from './img/github.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-name">
+          David Wang
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CS @ University of Waterloo
+          <br/>
+          BBA @ Wilfrid Laurier
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-now">
+          Currently: Applying for 2022 Winter Co-op
+        </div>
+        <div className="App-icons">
+          <a href="https://twitter.com/davidlwang" target="_blank">
+            <Twitter className="App-twitter" alt="twitter"/>
+            {/* <img src={twitter} className="App-twitter" alt="twitter" /> */}
+          </a>
+          |
+          <a href="https://github.com/da-wg" target="_blank">
+            <Github className="App-github" alt="github"/>
+            {/* <img src={github} className="App-github" alt="github" /> */}
+          </a>
+        </div>
       </header>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ReactComponent as Home } from './img/home.svg';
+
+import './Header.css';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="Header">
+                <p>
+                    <a href="../public/index.html">
+                        <Home className="Header-home" alt="home"/>
+                    </a>
+                </p>
+                {/* <div className="Header-nav">
+                    <a href="../public/about.html">
+                        About Me
+                    </a>
+                </div> */}
+            </div>
+        );
+    }
+}
+
+export default Header;
